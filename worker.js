31c5566023dca0e490bb39c40bd598b2ef7feea1
Shivam  Raj <shivam16@navgurukul.org>
2019-01-23 19:20:18 +0530

    Done and create CSV with respective data

--- worker.js
+++ worker.js
@@ -4,9 +4,9 @@ const Kue = require('kue');
 const queue = Kue.createQueue();
 
 const rp = require('request-promise');
-const otcsv = require('objects-to-csv');
 const cheerio = require('cheerio');
-
+const CsvWriter = require('csv-write-stream');
+const fs = require('fs');
 
 
 queue.process('scrap', function (job, done) {
@@ -16,29 +16,30 @@ queue.process('scrap', function (job, done) {
 
 const scraping_website = async (link) => {
     const html = await rp(link);
-    const businessMap = cheerio('a', html).map(async (i, e) => {
-        const second_link = e.attribs.href;
+    let filepath = '/tmp/Data.csv';
+    let writer = CsvWriter()
+    writer.pipe(fs.createWriteStream(filepath))
+    cheerio('a', html).map(async (i, e) => {
+        let second_link = e.attribs.href;
         if (second_link.includes('https://www.giveindia.org/nonprofit/') && second_link.length > 'https://www.giveindia.org/nonprofit/'.length) {
 
-            const innerHtml = await rp(second_link);
-            const address = cheerio('#__next > div:nth-child(2) > main > div > div.container > div > div.col-12.col-sm-12.col-md-7.col-lg-8 > div:nth-child(7) > div.w-100.row-margins.row > div.col-12.col-lg-5 > p span', innerHtml).text();
+            let innerHtml = await rp(second_link);
+            let address = cheerio('#__next > div:nth-child(2) > main > div > div.container > div > div.col-12.col-sm-12.col-md-7.col-lg-8 > div:nth-child(7) > div.w-100.row-margins.row > div.col-12.col-lg-5 > p span', innerHtml).text();
 
-            const email = cheerio('#__next > div:nth-child(2) > main > div > div.container > div > div.col-12.col-sm-12.col-md-7.col-lg-8 > div:nth-child(7) > div.w-100.row-margins.row > div.ml-auto.col-12.col-lg-6 > p:nth-child(2) a', innerHtml).text();
+            let email = cheerio('#__next > div:nth-child(2) > main > div > div.container > div > div.col-12.col-sm-12.col-md-7.col-lg-8 > div:nth-child(7) > div.w-100.row-margins.row > div.ml-auto.col-12.col-lg-6 > p:nth-child(2) a', innerHtml).text();
 
-            const phone = cheerio('#__next > div:nth-child(2) > main > div > div.container > div > div.col-12.col-sm-12.col-md-7.col-lg-8 > div:nth-child(7) > div.w-100.row-margins.row > div.ml-auto.col-12.col-lg-6 a').text()
+            let phone = cheerio('#__next > div:nth-child(2) > main > div > div.container > div > div.col-12.col-sm-12.col-md-7.col-lg-8 > div:nth-child(7) > div.w-100.row-margins.row > div.ml-auto.col-12.col-lg-6 > p:nth-child(1) a', innerHtml).text()
 
-            return {
-                "Email": email,
-                "Address": address,
-                "phone": phone
+            if (email.includes(" ")) {
+                email = email.split(" ")[0]
             }
+            let csv_row = {
+                "Email": email || "",
+                "Address": address || "",
+                "phone": phone || ""
+            }
+            writer.write(csv_row)
+            return csv_row
         }
     }).get();
-    Promise.all(businessMap)
-        .then((result) => {
-            console.log(result, "  final result")
-        })
-        .catch((err) => {
-            console.log("In error", err)
-        })
 };
